68eec5afa8e9538fd8a936675fe8ea59a248e2f3
Jan Hoek <jhoek@uncommonsense.nl>
2022-02-22 21:39:49 +0100

    Make items pipelinable

--- Public/New-RssFeed.ps1
+++ Public/New-RssFeed.ps1
@@ -28,28 +28,42 @@ function New-RssFeed
 
         [Nullable[DateTime]]$LastBuildDate,
 
+        [Parameter(ValueFromPipeline)]
         [RssItem[]]$Items
     )
 
-    $StringWriter = New-Object -TypeName System.IO.StringWriter
-    $XmlWriterSettings = New-Object -TypeName System.Xml.XmlWriterSettings
-    $XmlWriterSettings.Indent = $true
-    $XmlWriter = [System.Xml.XmlWriter]::Create($StringWriter, $XmlWriterSettings)
+    begin
+    {
+        $CachedItems = New-Object -TypeName System.Collections.Generic.List[RssItem]
+    }
 
-    $XmlWriter.WriteStartDocument()
-    $XmlWriter.WriteStartElement('rss')
-    $XmlWriter.WriteAttributeString('version', '2.0')
-    $XmlWriter.WriteStartElement('channel')
-    $XmlWriter.WriteElementString('title', $Title)
-    $XmlWriter.WriteElementString('link', $Link)
-    $XmlWriter.WriteElementString('description', $Description)
-    if ($PubDate) { $XmlWriter.WriteElementString('pubDate', $PubDate.ToString('ddd, d MMM yyyy HH:mm:ss zzz')) }
-    if ($LastBuildDate) { $XmlWriter.WriteElementString('lastBuildDate', $LastBuildDate.ToString('ddd, d MMM yyyy HH:mm:ss zzz')) }
-    $Items.ForEach{ $_.Write($XmlWriter) }
-    $XmlWriter.WriteEndElement() # channel
-    $XmlWriter.WriteEndElement() # rss
-    $XmlWriter.WriteEndDocument()
-    $XmlWriter.Flush()
+    process
+    {
+        $CachedItems.AddRange($Items)
+    }
 
-    $StringWriter.ToString()
+    end
+    {
+        $StringWriter = New-Object -TypeName System.IO.StringWriter
+        $XmlWriterSettings = New-Object -TypeName System.Xml.XmlWriterSettings
+        $XmlWriterSettings.Indent = $true
+        $XmlWriter = [System.Xml.XmlWriter]::Create($StringWriter, $XmlWriterSettings)
+
+        $XmlWriter.WriteStartDocument()
+        $XmlWriter.WriteStartElement('rss')
+        $XmlWriter.WriteAttributeString('version', '2.0')
+        $XmlWriter.WriteStartElement('channel')
+        $XmlWriter.WriteElementString('title', $Title)
+        $XmlWriter.WriteElementString('link', $Link)
+        $XmlWriter.WriteElementString('description', $Description)
+        if ($PubDate) { $XmlWriter.WriteElementString('pubDate', $PubDate.ToString('ddd, d MMM yyyy HH:mm:ss zzz')) }
+        if ($LastBuildDate) { $XmlWriter.WriteElementString('lastBuildDate', $LastBuildDate.ToString('ddd, d MMM yyyy HH:mm:ss zzz')) }
+        $CachedItems.ForEach{ $_.Write($XmlWriter) }
+        $XmlWriter.WriteEndElement() # channel
+        $XmlWriter.WriteEndElement() # rss
+        $XmlWriter.WriteEndDocument()
+        $XmlWriter.Flush()
+
+        $StringWriter.ToString()
+    }
 }
